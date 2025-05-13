ec8274aca64cff464371b600296177ca36ca5224
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-05-13 13:04:11 -0500

    New project initialisation

---
+++ 0-initialize_new_project.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create a new folder named 0-new_project
+mkdir 0-new_project
+
+# Navigate into the new project folder
+cd 0-new_project
+
+# Initialize a new C# console application
+dotnet new console
+
+# Run dotnet restore to restore any dependencies
+dotnet restore
+
+# Inform the user that the process is complete
+echo "The template 'Console Application' was created successfully."
