d826f42ff15379fed498828c9e12e9d38b66882b
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-05-13 14:22:22 -0500

    Fixing some file issues

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,16 +1,2 @@
 #!/usr/bin/env bash
-
-# Create a new folder named 0-new_project
-mkdir 0-new_project
-
-# Navigate into the new project folder
-cd 0-new_project
-
-# Initialize a new C# console application
-dotnet new console
-
-# Run dotnet restore to restore any dependencies
-dotnet restore
-
-# Inform the user that the process is complete
-echo "The template 'Console Application' was created successfully."
+dotnet new console -o 0-new_project
