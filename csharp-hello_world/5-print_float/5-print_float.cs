ec8f3d6966d9568a01b5d98661242e5b16494cc8
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-05-13 21:39:49 -0500

    Fix: Handle null values

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        float number = float.Parse(Console.ReadLine());
+        float number = float.Parse(Console.ReadLine() ?? "0");
         Console.WriteLine("Float: {0:F2}", number);
     }
 }
