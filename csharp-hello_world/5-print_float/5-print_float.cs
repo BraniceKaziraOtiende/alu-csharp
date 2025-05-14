5b4a8470e31dc456d06e8eaa1146dbc7b4f9604d
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-05-13 21:45:03 -0500

    Fix file 5

--- csharp-hello_world/5-print_float/5-print_float.cs
+++ csharp-hello_world/5-print_float/5-print_float.cs
@@ -4,7 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        float number = float.Parse(Console.ReadLine() ?? "0");
+        string input = Console.ReadLine();
+        float number = 0;
+        if (!float.TryParse(input, out number)) number = 0;
         Console.WriteLine("Float: {0:F2}", number);
     }
 }
