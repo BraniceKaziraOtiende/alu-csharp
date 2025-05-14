59a33a450ed8b43d4426ff763288a808077fb09a
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-05-13 22:01:50 -0500

    Fix:File 6

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -4,9 +4,9 @@ class Program
     static void Main(string[] args)
     {
         string str = "Holberton School";
-        Console.WriteLine(str);  // Print the string 1st time
-        Console.WriteLine(str);  // Print the string 2nd time
-        Console.WriteLine(str);  // Print the string 3rd time
-        Console.WriteLine(str.Substring(0, 9));  // Print the first 9 characters
+        Console.Write(str);  // Print the string 1st time without newline
+        Console.Write(str);  // Print the string 2nd time without newline
+        Console.Write(str);  // Print the string 3rd time without newline
+        Console.WriteLine(str.Substring(0, 9));  // Print the first 9 characters with newline
     }
 }
