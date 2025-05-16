5cca25ae20df2a8fe66426bca0f61092de0ac669
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-05-15 23:10:33 -0700

    Add file 7

---
+++ csharp-ifelse_loops_methods/7-islower/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string result = "";
+
+        for (int i = 0; i <= 8; i++)
+        {
+            for (int j = i + 1; j <= 9; j++)
+            {
+                result += $"{i}{j}, ";
+            }
+        }
+
+        // Remove the last comma and space, then print
+        Console.WriteLine(result.TrimEnd(',', ' '));
+    }
+}
