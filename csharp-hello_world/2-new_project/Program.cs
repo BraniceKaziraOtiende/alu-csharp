eaf4c186004896b6110750076682f3dbf8d20898
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-05-16 00:22:35 -0700

    Attempted fix

--- csharp-hello_world/2-new_project/Program.cs
+++ csharp-hello_world/2-new_project/Program.cs
@@ -1,7 +1,2 @@
-﻿class Program
-{
-    static void Main()
-    {
-        System.Console.Write("Hello World!");
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,9 +5,7 @@ class Program
     static void Main(string[] args)
     {
         string str = "Holberton School";
-        Console.Write(str);  // Print the string 1st time without newline
-        Console.Write(str);  // Print the string 2nd time without newline
-        Console.Write(str);  // Print the string 3rd time without newline
-        Console.WriteLine(str.Substring(0, 9));  // Print the first 9 characters with newline
+        Console.WriteLine("{0}{0}{0}", str);
+        Console.WriteLine(str.Substring(0, 9));
     }
 }
