6effbfbb2726f22d81c19635de1a74d4f01e252f
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-06-01 15:59:46 +0200

    fix file 6

--- csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -1,20 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace MyUniqueNamespace
+class List
 {
-    public class List
+    public static List<bool> DivisibleBy2(List<int> myList)
     {
-        public static List<bool> DivisibleBy2(List<int> myList)
+        List<bool> result = new List<bool>();
+
+        foreach (int num in myList)
         {
-            List<bool> result = new List<bool>();
+            result.Add(num % 2 == 0);
+        }
 
-            foreach (int number in myList)
-            {
-                result.Add(number % 2 == 0);
-            }
+        return result;
+    }
+}
 
-            return result;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> myList = new List<int>() { 1, 2, 3, 4, 5, 6 };
+        List<bool> result = new List<bool>();
+        int i;
+
+        result = List.DivisibleBy2(myList);
+
+        for (i = 0; i < myList.Count; i++)
+        {
+            if (result[i] == true)
+                Console.WriteLine(myList[i] + " is divisible by 2");
+            else
+                Console.WriteLine(myList[i] + " is not divisible by 2");
         }
     }
 }
