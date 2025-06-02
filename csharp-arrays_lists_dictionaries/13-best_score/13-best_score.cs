3f31691fc7bf86e55a764bf8f7fbdb22e5ff428c
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-06-02 12:04:03 +0200

    Fix file 13

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -1,25 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 
-class Dictionary
+public class Dictionary
 {
-    public static string BestScore(System.Collections.Generic.Dictionary<string, int> myList)
+    public static string BestScore(Dictionary<string, int> myList)
     {
-        if (myList == null || myList.Count == 0)
+        if (myList.Count == 0)
             return "None";
 
-        string bestKey = null;
-        int maxScore = -1;
+        string bestKey = "";
+        int maxScore = int.MinValue;
 
-        foreach (var kvp in myList)
+        foreach (KeyValuePair<string, int> entry in myList)
         {
-            if (kvp.Value > maxScore)
+            if (entry.Value > maxScore)
             {
-                maxScore = kvp.Value;
-                bestKey = kvp.Key;
+                maxScore = entry.Value;
+                bestKey = entry.Key;
             }
         }
 
-        return bestKey ?? "None";
+        return bestKey;
     }
 }
