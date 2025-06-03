322a9fdce0ad667cb418e7484fde810d1eac24f9
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-06-04 00:03:35 +0200

    File 2

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -5,37 +5,34 @@ public class List
 {
     public static List<int> DifferentElements(List<int> list1, List<int> list2)
     {
-        HashSet<int> set1 = new HashSet<int>(list1);
-        HashSet<int> set2 = new HashSet<int>(list2);
-        HashSet<int> resultSet = new HashSet<int>();
+        List<int> result = new List<int>();
 
-        // Add elements in set1 but not in set2
-        foreach (int num in set1)
+        // Add elements from list1 that are not in list2
+        foreach (int num in list1)
         {
-            if (!set2.Contains(num))
+            if (!list2.Contains(num))
             {
-                resultSet.Add(num);
+                result.Add(num);
             }
         }
 
-        // Add elements in set2 but not in set1
-        foreach (int num in set2)
+        // Add elements from list2 that are not in list1
+        foreach (int num in list2)
         {
-            if (!set1.Contains(num))
+            if (!list1.Contains(num))
             {
-                resultSet.Add(num);
+                result.Add(num);
             }
         }
 
-        List<int> result = new List<int>(resultSet);
-
-        // Manual sorting using Bubble Sort (since LINQ and SortedList not allowed)
+        // Sort the result list using bubble sort
         for (int i = 0; i < result.Count - 1; i++)
         {
             for (int j = 0; j < result.Count - i - 1; j++)
             {
                 if (result[j] > result[j + 1])
                 {
+                    // Swap elements
                     int temp = result[j];
                     result[j] = result[j + 1];
                     result[j + 1] = temp;
