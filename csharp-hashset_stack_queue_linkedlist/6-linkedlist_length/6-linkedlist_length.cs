29160d86725c5f1b075adc735822c0af7bfa97e7
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-06-03 23:38:16 +0200

    File 6

--- csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
+++ csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -1,18 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 public class LList
 {
     public static int Length(LinkedList<int> myLList)
     {
         int count = 0;
-        LinkedListNode<int> current = myLList.First;
-
-        while (current != null)
+        LinkedListNode<int> currentNode = myLList.First;
+        while (currentNode != null)
         {
             count++;
-            current = current.Next;
+            currentNode = currentNode.Next;
         }
-
         return count;
     }
 }
