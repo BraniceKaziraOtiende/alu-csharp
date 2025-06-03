01dedf1552bc0cf1ca2a624ac89c5fb094947a96
BraniceKaziraOtiende <branicekazira@gmail.com>
2025-06-04 00:07:56 +0200

    File 10

--- csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -5,12 +5,16 @@ public class LList
 {
     public static int GetNode(LinkedList<int> myLList, int n)
     {
+        // Check if index is valid
         if (n < 0 || myLList.Count <= n)
         {
             return 0;
         }
 
+        // Start at first node
         LinkedListNode<int> currentNode = myLList.First;
+        
+        // Move n positions forward
         for (int i = 0; i < n; i++)
         {
             currentNode = currentNode.Next;
